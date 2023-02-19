579952b6847b20b6b4815e8150e476e7fa7a42b5
luismvalderrama <luismvalderrama.01@gmail.com>
2023-02-19 00:16:58 -0500

    feat: adjust next slide function

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -24,14 +24,8 @@ function updateSlidePosition() {
     slide.classList.add('hidden');
   });
 
-
-  // Using the .forEach array method, (array.forEach((element) => { per-element work goes here }))
-  // loop through all the slides in your slideArray
-  // and remove the 'visible' class from each classList
-  // then add a class 'hidden' to all of them
-
-  // outside your .forEach,
-  // add a 'visible' class to the slide at the current slidePosition in slides
+  console.log(slidePosition);
+  slides[slidePosition].classList.add('visible');
 }
 
 function moveToNextSlide() {
@@ -41,6 +35,13 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+
+  if(slidePosition === totalSlides - 1){
+    slidePosition = 0;
+   } else {
+    slidePosition += 1; 
+   }
+
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
