428d63185ceee0057d859c0634398c5bc5e16dcb
luismvalderrama <luismvalderrama.01@gmail.com>
2023-03-20 00:29:30 -0400

    feat: add cutRestaurantList function/eventListener

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -15,34 +15,35 @@ function injectHTML(list) {
   })
 }
 
-
-
-
 /* A quick filter that will return something based on a matching input */
 function filterList(list, query) {
   return list.filter((item)=> {
     const lowerCaseName =item.name.toLowerCase();
     const lowerCaseQuery = query.toLowerCase();
     return lowerCaseName.includes(lowerCaseQuery);
-  })
-  /*
-    Using the .filter array method, 
-    return a list that is filtered by comparing the item name in lower case
-    to the query in lower case
+  });
+}
 
-    Ask the TAs if you need help with this
-  */
+function cutRestaurantList(list) {
+  console.log('fired cut list');
+  const range = [...Array(15).keys()];
+  return newArray = range.map((item) => {
+    const index = getRandomIntInclusive(0, list.length -1);
+    return list[index]
+  })
 }
 
 async function mainEvent() { // the async keyword means we can make API requests
   const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-  const filterButton = document.querySelector('.filter_button');
+  const filterButton = document.querySelector('#filter');
+  const loadDataButton = document.querySelector('#data_load');
+  const generateListButton = document.querySelector('#generate');
   // Add a querySelector that targets your filter button here
 
   let currentList = []; // this is "scoped" to the main event function
   
   /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
-  mainForm.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+  loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
     
     // This prevents your page from becoming a list of 1000 records from the county, even if your form still has an action set on it
     submitEvent.preventDefault(); 
@@ -56,7 +57,6 @@ async function mainEvent() { // the async keyword means we can make API requests
     // This changes the response from the GET into data we can use - an "object"
     currentList = await results.json();
     console.table(currentList); 
-    injectHTML(currentList); 
   });
 
   filterButton.addEventListener('click', (event) => {
@@ -71,17 +71,13 @@ async function mainEvent() { // the async keyword means we can make API requests
 
     console.log(newList);
   })
-  /*
-    Now that you HAVE a list loaded, write an event listener set to your filter button
-    it should use the 'new FormData(target-form)' method to read the contents of your main form
-    and the Object.fromEntries() method to convert that data to an object we can work with
 
-    When you have the contents of the form, use the placeholder at line 7
-    to write a list filter
-
-    Fire it here and filter for the word "pizza"
-    you should get approximately 46 results
-  */
+  generateListButton.addEventListener('click', (event) => {
+    console.log('generate new list');
+    const restaurantList = cutRestaurantList(currentList);
+    console.log(restaurantList);
+    injectHTML(restaurantList);
+  })
 }
 
 /*
